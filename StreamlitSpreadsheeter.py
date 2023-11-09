415cca4f28e70eefed401d705de6d909d779df25
KeirPlaice <150185289+KeirPlaice@users.noreply.github.com>
2023-11-09 08:33:10 +0100

    Update StreamlitSpreadsheeter.py

--- StreamlitSpreadsheeter.py
+++ StreamlitSpreadsheeter.py
@@ -12,14 +12,14 @@ st.write('For demonstration purposes, a temporary API key and Search Engine ID a
 
 API=st.text_input(
         "What is your API key?",
-        "AIzaSyBB-UxitlFtIbTMrdcBkNeY44sPa7E9puY",
+        "st.secrets["API_key"]",
         key="APIplaceholder",
     )
 
 
 ID=st.text_input(
         'What is your Google Custom Search Engine ID?',
-        "e6e23df52086e4ed7",
+        "st.secrets[SE_ID]",
         key="IDplaceholder",
     )
 
